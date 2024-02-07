63e7e14f18ad65e92f8c847ee99c387f09579190
Thiago <thiagogoes1011@gmail.com>
2024-02-07 08:44:18 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
-const mensagemCriptografada = "10010110 11110111 01010110 00000001 00010111 00100110 01010111 00000001 00010111 01110110 01010111 00110110 11110111 11010111 01010111 00000011";
-const arrayLetrasBinarias = mensagemCriptografada.split(" ");
-
-let arrayLetrasBinariasEditada = [];
-
-for (let i = 0; i < arrayLetrasBinarias.length; i++) {
-    const item = arrayLetrasBinarias[i];
-    console.log("item: " + item);
-    const parte1 = item.slice(0, item.length / 2);
-    console.log("parte1: " + parte1);
-    const parte2 = item.slice(item.length / 2, item.length);
-    console.log("parte2: " + parte2);
-    const arrayParte2 = parte2.split("");
-
-    console.log("arrayParte2: " + arrayParte2);
-
-    const penultimo = parte2.split("")[parte2.length - 2];
-    const ultimo = parte2.split("")[parte2.length - 1];
-
-    arrayParte2[parte2.length - 2] = ultimo;
-    arrayParte2[parte2.length - 1] = penultimo;
-
-    console.log("arrayParte2 editada: " + arrayParte2);
-    
-
-    const novoItem = arrayParte2.join("") +  parte1;
-    console.log("novoItem: " + novoItem);
-
-    arrayLetrasBinariasEditada.push(novoItem);
-    
+function binarioParaTexto(ListaBinarios) {
+    return ListaBinarios.map(bloco => String.fromCharCode(parseInt(bloco, 2))).join("");
 }
 
-console.log("mesangem: " + arrayLetrasBinariasEditada.join(" "));
+function chaveDeDecodificacao(mensagem){
+    const arrayLetrasBinarias = mensagem.split(" ");
+    let arrayLetrasBinariasDecodificado = [];
+
+    for (let i = 0; i < arrayLetrasBinarias.length; i++) {
+        const itemDoArray = arrayLetrasBinarias[i];
+        
+        const parte1 = itemDoArray.slice(0, itemDoArray.length / 2);
+        const parte2 = itemDoArray.slice(itemDoArray.length / 2, itemDoArray.length);
+        
+        const arrayParte2 = parte2.split("");
+    
+        arrayParte2[parte2.length - 2] = arrayParte2[parte2.length - 2] == "1"? "0": "1";
+        arrayParte2[parte2.length - 1] = arrayParte2[parte2.length - 1] == "1"? "0": "1";
+
+        const BinarioParte2ComOsUltimosInvertidos = arrayParte2.join("");
+    
+        const novoItemTrocandoPartesDeLugar =  BinarioParte2ComOsUltimosInvertidos +  parte1;
+    
+        arrayLetrasBinariasDecodificado.push(novoItemTrocandoPartesDeLugar);
+    }
+    return arrayLetrasBinariasDecodificado;
+}
+
+function exibeNaTela(){
+
+    const elementCriptograda = document.createElement("p");
+    const elementDescriptografada = document.createElement("p");
+    const elementMensagem = document.createElement("p");
+    const elementAviso = document.createElement("p");
+
+    elementCriptograda.innerHTML = "Mensagem Criptografada: " +  mensagemCriptografada;
+    elementDescriptografada.innerHTML = "Mensagem Descriptograda: " +  arrayBinarioDecodificado.join(" ");
+    elementMensagem.innerHTML = "Mensagem: " + texto;
+    elementAviso.innerHTML = "Resultado Disponível Também no console!!"
+    elementAviso.style = "font-weight: bold; font-size: 1.5em;"
+    const container = document.getElementById("container");
+
+    container.appendChild(elementCriptograda);
+    container.appendChild(elementDescriptografada);
+    container.appendChild(elementMensagem);
+    container.appendChild(elementAviso);
+}
+
+
+const mensagemCriptografada = "10010110 11110111 01010110 00000001 00010111 00100110 01010111 00000001 00010111 01110110 01010111 00110110 11110111 11010111 01010111 00000011";
+
+const arrayBinarioDecodificado = chaveDeDecodificacao(mensagemCriptografada)
+const texto = binarioParaTexto(arrayBinarioDecodificado);
+console.log("Mensagem Criptografada: " + mensagemCriptografada);
+console.log("Mensagem Descriptograda: " + arrayBinarioDecodificado);
+console.log("Mensagem: " + texto);
+
+exibeNaTela();
+
+
